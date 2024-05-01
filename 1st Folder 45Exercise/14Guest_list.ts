9866e6c69e74311a81cd55b391d7e630dfd65430
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 02:15:53 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/14Guest_list.ts
@@ -0,0 +1,13 @@
+
+
+let Guest_List :string[] = ['Imran khan', 'Nawaz sharef','Zardari'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+
+console.log('Dear Mr> ' +Guest_List[i] + ',\n\nIt is Our pleasure to invite  you  in our party. \n\nThank You!')
+
+}
+
+
+
