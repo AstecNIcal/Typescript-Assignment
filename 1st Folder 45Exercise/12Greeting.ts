dc998d617d453e0e876b2407e26c0cad65e9b49a
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 02:13:11 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/12Greeting.ts
@@ -0,0 +1,16 @@
+
+
+let names :string[] = ['asma','faiza','kashaf','zareen',];
+
+const  message :string = 'Welcome to Typescript class:'
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]);
+}
+
+
+
+
+
+
