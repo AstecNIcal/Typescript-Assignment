3366f13ad206bb6d3c2c546934d60d0befeaa838
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 01:59:21 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/6stripping_names.ts
@@ -0,0 +1,12 @@
+
+const personName :string = '   \n\tshahid Afridi\t\n   ';
+console.log(personName);
+
+const without_whitespace :string = personName.trim();
+
+console.log(without_whitespace);
+
+
+
+
+
