978d656a16da17029494d16ea98603c8938ca9ca
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 02:14:28 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/13Your_own__Array.ts
@@ -0,0 +1,14 @@
+
+
+let transportation :string[] = ['motorcycle','car','Train','cycle'] ;
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a ' + transportation[i])
+
+}
+
+
+
+
+
