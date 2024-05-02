651da92ff8389ca4f9134658beeaf915fcf77d3e
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 15:38:09 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/21Typescript_object.ts
@@ -0,0 +1,71 @@
+
+// write a program that creates objects containing these items.
+
+// let person_Name :string = 'As';
+
+
+// const personName_Array :string [] = ['person','cycle','7up drink'];
+
+interface person {
+age : number,
+name : string,
+nationality : string,
+student : Boolean
+
+}
+
+let person :person = {
+age : 26 ,
+name : 'farhan' ,
+nationality : 'pakistan' ,
+student : true
+
+}
+
+console.log(person);
+
+// Datatype of car object
+interface car{ 
+  maker : string,
+  color : string,
+  automatic : boolean
+}
+
+// car object
+let car ={
+  maker : 'toyato',
+  color : 'Black',
+  automatic : true
+}
+
+// print car
+console.log(car);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
