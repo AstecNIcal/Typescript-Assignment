4951d6adf27f2f71c1992ecbf643ff32afefef24
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 15:39:14 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/22International_Error.ts
@@ -0,0 +1,25 @@
+
+// if you haven not recived an array index error in one of your program yet, try to make one happen.
+
+
+// creating array
+let newArray :string[] =['Will smith','james Bond','Tom Cruise'];
+
+// Accessing array by index
+console.log(newArray[5]);
+
+//Make sure you correct the error before closing the program.
+
+// correct the error
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
