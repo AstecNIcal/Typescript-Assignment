15dc973ab19e6e2e929b37ad205d762fbd50add7
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 15:53:46 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/31No_Users.ts
@@ -0,0 +1,44 @@
+
+
+let userNames: string[] = ['admin','sohail','shajahan','shariq','zia'];
+
+if(userNames.length === 0){
+  console.log('we need  to find some users!');
+
+}else{
+
+userNames = [];
+ console.log('All user Names have been removed.'+ userNames.length);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
