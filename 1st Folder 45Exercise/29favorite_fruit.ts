1a00c77f8821140178d4fb674b7a770752f14e5c
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 15:48:29 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/29favorite_fruit.ts
@@ -0,0 +1,42 @@
+// favorite fruit: Make a array of your favorite fruits, and write a series of independent if statements that check for certain fruits in your array.
+// . Make a array of your three favorite fruits and call it favorite_fruits.
+// . write five if statements.Eash should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as you really like banana !
+
+
+let favorite_fruits: string[] = ['apple','banana','mango'];
+
+if(favorite_fruits.includes('apple')){
+     console.log('i really like apple!');
+
+
+}
+
+if(favorite_fruits.includes('banana')){
+    console.log('i really like banana!');
+}
+
+
+ if(favorite_fruits.includes('mango')){
+     console.log('i really like mango!');
+ }
+    
+
+
+ if(favorite_fruits.includes('orange')){
+      console.log('i really like orange!');
+ }      
+
+
+ if(favorite_fruits.includes('straberry')){
+    console.log('i really like strawberry!');
+}      
+
+
+
+
+
+
+
+
+
+
