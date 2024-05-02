89b4f0562de3af68f1269cba098c46b69b58716f
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-05-02 15:35:48 +0500

    Add files via upload

---
+++ 1st Folder 45Exercise/20Empty_Array.ts
@@ -0,0 +1,17 @@
+
+//Think of something you colud store in a array.
+let iteams :string[ ] = ['pakistan','karachi', ' urdu'];
+
+//write a program that creates a list containing these iteam.
+let item2 :string[] = [] ;
+
+item2.push('Canada');
+item2.push('india');
+item2.push('England');
+
+console.log(item2);
+
+
+
+
+
